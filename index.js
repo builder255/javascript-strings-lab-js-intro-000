53a2e6ccfdf622a6d586156ec1c2f206eeb8f206
Rich D <musiclike@hotmail.com>
2021-01-16 02:18:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ var specialGuest = "Neil deGrasse Tyson"
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
-Lets talk about space.
 
 var conversation = `${topic}`;
++Lets talk about space.
